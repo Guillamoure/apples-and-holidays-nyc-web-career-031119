a0c02fab1865a08b94217652e38880655733adb7
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-28 02:20:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, holiday|
+    i = 0
+    while i < holiday.keys.length
+      if holiday.values.include?
+    i+=1
+    end
+  end
 end
 
 
